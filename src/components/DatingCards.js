98f21b3399ec459da25242eab8791ced71c22445
Danmainah <danielmainawaweru@gmail.com>
2024-03-11 18:50:13 +0300

    fix build  error

--- src/components/DatingCards.js
+++ src/components/DatingCards.js
@@ -1,40 +1,43 @@
-import React, { useState, useEffect } from 'react'
-import DatingCard from 'react-tinder-card'
-import axios from './axios'
-import './DatingCards.css'
+import React, { useState, useEffect } from "react";
+import DatingCard from "react-tinder-card";
+import axios from "./axios";
+import "./DatingCards.css";
 const DatingCards = () => {
-    const [people, setPeople] = useState([])
-    useEffect(() => {
+  const [people, setPeople] = useState([]);
+  useEffect(() => {
     async function fetchData() {
-    const req = await axios.get("/dating/cards")
-    setPeople(req.data)
+      const req = await axios.get("/dating/cards");
+      setPeople(req.data);
     }
-    fetchData()
-    }, [])
-const swiped = (direction, nameToDelete) => {
-console.log("receiving " + nameToDelete)
-}
-const outOfFrame = (name) => {
-console.log(name + " left the screen!!")
-}
-return (
-<div className="datingCards">
-<div className="datingCards__container">
-{people.map((person) => (
-<DatingCard
-className="swipe"
-key={person.name}
-preventSwipe={['up', 'down']}
-onSwipe={(dir) => swiped(dir, person.name)}
-onCardLeftScreen={() => outOfFrame(person.name)} >
-<div style={{ backgroundImage: `url(${person.
-imgUrl})`}} className="card">
-<h3>{person.name}</h3>
-</div>
-</DatingCard>
-))}
-</div>
-</div>
-)
-}
-export default DatingCards
+    fetchData();
+  }, []);
+  const swiped = (direction, nameToDelete) => {
+    console.log("receiving " + nameToDelete);
+  };
+  const outOfFrame = (name) => {
+    console.log(name + " left the screen!!");
+  };
+  return (
+    <div className="datingCards">
+      <div className="datingCards__container">
+        {people.map((person) => (
+          <DatingCard
+            className="swipe"
+            key={person.name}
+            preventSwipe={["up", "down"]}
+            onSwipe={(dir) => swiped(dir, person.name)}
+            onCardLeftScreen={() => outOfFrame(person.name)}
+          >
+            <div
+              style={{ backgroundImage: `url(${person.imgUrl})` }}
+              className="card"
+            >
+              <h3>{person.name}</h3>
+            </div>
+          </DatingCard>
+        ))}
+      </div>
+    </div>
+  );
+};
+export default DatingCards;
